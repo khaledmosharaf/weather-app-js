ac40db5871f10f33b4abb3565426710dc2207b7b
khaledmosharaf <khaledmosharaf999@gmail.com>
2022-04-11 01:13:00 +0600

    initial

--- weather.js
+++ weather.js
@@ -1,49 +1,49 @@
 class City {
-  
-  constructor (city) {
-    this.apiKey = '44c84272af3bcf9ec72717da4289f2eb';
-    this.city = city;
-    this.responseLimit = 5;
+  constructor(city) {
+    this.apiKey = '44c84272af3bcf9ec72717da4289f2eb'
+    this.city = city
+    this.responseLimit = 5
   }
 
   // Fetch country from api
 
-  async getCity(){
-    const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${this.city}&limit=${this.responseLimit}&appid=${this.apiKey}`);
+  async getCity() {
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${this.city}&limit=${this.responseLimit}&appid=${this.apiKey}`
+    )
 
-    const responseData = await response.json();
+    const responseData = await response.json()
 
-    console.log(responseData);
+    console.log(responseData)
 
-    return responseData;
+    return responseData
   }
 
-  changeCity (city) {
-    this.city = city;
+  changeCity(city) {
+    this.city = city
   }
 }
 
 class Weather {
-  
-  constructor (lat, lon) {
-    this.apiKey = '44c84272af3bcf9ec72717da4289f2eb';
-    this.lat = lat;
-    this.lon = lon;
+  constructor(lat, lon) {
+    this.apiKey = '44c84272af3bcf9ec72717da4289f2eb'
+    this.lat = lat
+    this.lon = lon
   }
 
   async getWeather() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`)
+    const response = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?lat=${this.lat}&lon=${this.lon}&appid=${this.apiKey}&units=metric`
+    )
 
-    const responseData = await response.json();
+    const responseData = await response.json()
 
-    console.log(responseData);
-    return responseData;
-
+    console.log(responseData)
+    return responseData
   }
 
   changeCity(lat, lon) {
-    this.lat = lat;
-    this.lon = lon;
+    this.lat = lat
+    this.lon = lon
   }
 }
-
